59f5cd9b1e7f23f34896a8006a23f7d7917e15c9
Leonardo <leonardohibanhez@gmail.com>
2026-02-04 12:28:26 -0300

    :sparkles: Construção da Classe Conta

--- Menu.ts
+++ Menu.ts
@@ -1,8 +1,30 @@
 import leia from "readline-sync";
 import { colors } from "./src/util/Colors";
+import { Conta } from "./src/model/Conta";
 
 export function main() {
     let opcao: number;
+
+    //INSTANCIAR OBJETOS DA CLASSE CONTA
+
+    const c1 = new Conta(1 ,1234 ,"Sofia", 1 , 1000000.00);;
+
+    c1.visualizar();
+
+
+    //TESTES DO MÉTODO SACAR
+    console.log("Sacar 100,00", c1.sacar(100.00));
+    console.log("Sacar 30000000000.00", c1.sacar(30000000000.00));
+    console.log("Sacar 0,00", c1.sacar(0.00));
+
+    //TESTES DO MÉTODO DEPOSITAR
+    console.log("Depositar - 10,00")
+    c1.depositar(-10.00)
+
+    console.log("Depositar 500,00");
+    c1.depositar(500.00)
+
+    c1.visualizar();
 
     do {
 
@@ -74,6 +96,8 @@ export function main() {
     } while (true)
 
 }
+//ÁREA DE TESTE
+
 function sobre(): void {
     console.log(colors.fg.yellow,
         "\n*****************************************************");
--- src/model/Conta.ts
+++ src/model/Conta.ts
@@ -1,3 +1,133 @@
-export class Conta{
-    
+import { colors } from "../util/Colors";
+
+export class Conta {
+
+    //ATRIBUTOS DA CLASSE
+
+    private _numero: number;
+    private _agencia: number;
+    private _titular: string;
+    private _tipo: number;
+    private _saldo: number;
+
+    //METODO CONSTRUTOR
+
+    constructor(numero: number, agencia: number, titular: string, tipo: number, saldo: number) {
+        this._numero = numero;
+        this._agencia = agencia;
+        this._titular = titular;
+        this._tipo = tipo;
+        this._saldo = saldo;
+    }
+
+
+    //MÉTODOS GET E SET
+
+    public get numero(): number {
+        return this._numero;
+    }
+
+
+    public get agencia(): number {
+        return this._agencia;
+    }
+
+
+    public get titular(): string {
+        return this._titular;
+    }
+
+
+    public get tipo(): number {
+        return this._tipo;
+    }
+
+
+    public get saldo(): number {
+        return this._saldo;
+    }
+
+
+    public set numero(value: number) {
+        this._numero = value;
+    }
+
+
+    public set agencia(value: number) {
+        this._agencia = value;
+    }
+
+
+    public set titular(value: string) {
+        this._titular = value;
+    }
+
+
+    public set tipo(value: number) {
+        this._tipo = value;
+    }
+
+
+    public set saldo(value: number) {
+        this._saldo = value;
+    }
+
+
+    //METODOS AUXILIARES
+
+    public sacar(valor: number): boolean {
+
+        if (valor <= 0) {
+            console.log(colors.fg.red, "O valor deve ser Positivo!", colors.reset);
+            return false;
+        }
+
+
+        if (valor > this._saldo) {
+            console.log(colors.fg.red, "Saldo Insuficiente!", colors.reset);
+            return false;
+        }
+
+        this._saldo -= valor;
+        return true;
+    }
+
+    public depositar(valor: number): void {
+
+        if (valor <= 0)
+            console.log(colors.fg.red, "O valor deve ser Positivo!", colors.reset);
+
+        else
+            this._saldo += valor;
+    }
+
+
+    public visualizar(): void {
+
+        let tipo: string;
+
+        switch (this._tipo) {
+            case 1:
+                tipo = "Conta Corrente"
+                break;
+
+            case 2:
+                tipo = "Conta Poupança"
+                break;
+            default:
+                tipo = "Tipo Inválido"
+
+        }
+
+
+        console.log("\n*************************************");
+        console.log("           DADOS DA CONTA              ");
+        console.log("*************************************  ");
+        console.log(`Numero da conta: ${this._numero}       `);
+        console.log(`Agencia : ${this._agencia}      `);
+        console.log(`Titular da Conta: ${this._titular}      `);
+        console.log(`Tipo da Conta: ${tipo}               `);
+        console.log(`Saldo da conta: R$ ${this._saldo.toFixed(2)}`);
+    }
+
 }
